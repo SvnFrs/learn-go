cad43cffefebcb312522aeab2e91cec26bf446cd
“SvnFrs” <thaidvqce170410@fpt.edu.vn>
2023-12-05 23:09:40 +0700

    Still dont know how map works

--- first/hello/hello.go
+++ first/hello/hello.go
@@ -11,11 +11,18 @@ func main() {
 	log.SetPrefix("Error: ")
 	log.SetFlags(0)
 
-	fmt.Println("Please enter your name!")
-	var name string
-	fmt.Scanf("%v", &name)
+	fmt.Println("How many people are there?")
+	var number int
+	fmt.Scanf("%d", &number)
 
-	message, err := greetings.Hello(name)
+	var name []string
+	for i := 0; i < number; i++ {
+		fmt.Println("Please enter your name!")
+		var input string
+		fmt.Scanf("%s", &input)
+		name = append(name, input)
+	}
+	message, err := greetings.Hellos(name)
 
 	if err != nil {
 		log.Fatal(err)
