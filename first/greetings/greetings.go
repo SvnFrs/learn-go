3df34f25731fc13e1dc9df34e25b3ddfb7a4af0e
“SvnFrs” <thaidvqce170410@fpt.edu.vn>
2023-12-05 22:18:43 +0700

    Improve first

--- first/greetings/greetings.go
+++ first/greetings/greetings.go
@@ -1,10 +1,15 @@
 package greetings
 
 import (
+	"errors"
 	"fmt"
 )
 
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("consider writing a proper name next time")
+	}
+
 	message := fmt.Sprintf("Hi %v, Welcome to Go!", name)
-	return message
+	return message, nil
 }
--- first/hello/hello.go
+++ first/hello/hello.go
@@ -2,15 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
+	log.SetPrefix("Error: ")
+	log.SetFlags(0)
+
 	fmt.Println("Please enter your name!")
 	var name string
 	fmt.Scanf("%v", &name)
 
-	message := greetings.Hello(name)
+	message, err := greetings.Hello(name)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
