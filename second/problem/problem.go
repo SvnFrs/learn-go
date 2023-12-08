456359c6e39529d3dc4a9bf0657dafdf20ef4288
“SvnFrs” <thaidvqce170410@fpt.edu.vn>
2023-12-08 21:57:58 +0700

    Do some basic things

--- second/problem/problem.go
+++ second/problem/problem.go
@@ -3,11 +3,15 @@ package main
 import (
 	"fmt"
 	"math"
+
+	"example.com/random"
 )
 
 func main() {
-	var problems float32
-	fmt.Println("How many problem do you have, I'll cut it in half")
-	fmt.Scanf("%g", &problems)
+	// var problems float32
+	// fmt.Println("How many problem do you have, I'll cut it in half")
+	// fmt.Scanf("%g", &problems)
+	problems := random.CreateRandomNumbers()
+	fmt.Printf("You have a total of %d problems\n", problems)
 	fmt.Printf("Now you have %g problems. Thank me later\n", math.Sqrt(float64(problems)))
 }
--- second/random/random.go
+++ second/random/random.go
@@ -1,10 +1,9 @@
-package main
+package random
 
 import (
-	"fmt"
 	"math/rand"
 )
 
-func main() {
-	fmt.Printf("My favorite number is %d", rand.Intn(10))
+func CreateRandomNumbers() int {
+	return rand.Intn(100)
 }
