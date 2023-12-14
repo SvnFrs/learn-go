56116ce8129e9401cd3df4208692e831ac736eaa
“SvnFrs” <thaidvqce170410@fpt.edu.vn>
2023-12-14 14:51:36 +0700

    First version of the bot

---
+++ go-gopher-bot-discord/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func main() {
+	sess, err := discordgo.New("Bot MTE4NDc0ODYwMjA4NjY1NDAzMw.GP-s6-.q6sCewGiH_HfxvcTU-hdICv-kG5LBZsYdalnTI")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sess.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		if m.Author.ID == s.State.User.ID {
+			return
+		}
+
+		if m.Content == "hello" {
+			s.ChannelMessageSend(m.ChannelID, "world!")
+		}
+		if m.Content == "cum" {
+			s.ChannelMessageSend(m.ChannelID, "Inside u")
+		}
+	})
+
+	sess.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
+
+	err = sess.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer sess.Close()
+
+	fmt.Println("THe bot is online")
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+}
